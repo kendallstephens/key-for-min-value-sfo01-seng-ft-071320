be550f3cc9d413cc705ca2fee1e5686e9ce27d1b
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-06 17:32:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    num = 0
+  smallest_value = ""
+  name_hash.each do |key, value|
+    if value > smallest_value = 1000
+smallest_value = key
+  end
+end
+smallest_value
 end
